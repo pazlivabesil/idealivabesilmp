3ea6a0d696b201ffc8a6810f2981041e518d62eb
PAZ <paz.livabesil@gmail.com>
2022-05-16 23:29:32 -0300

    Clase de jsx-webpack

---
+++ src/Components/NavBar.js/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar =  () => {
+    return (
+        <nav>
+            <h1>All4U</h1>
+            <div>
+                <button>Chocolates</button>
+                <button>Bebidas alcoholicas</button>
+                <button>Bebidas analcoholicas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
