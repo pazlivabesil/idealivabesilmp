4ce16fc814ae530fdf131aa7837de938cfaded26
PAZ <paz.livabesil@gmail.com>
2022-05-25 21:10:26 -0300

    Clase Componentes 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar.js/NavBar';
-import CountFunction from './Components/CountFunction/CountFunction'; 
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import ItemCount from './Components/ItemCount/ItemCount';
+
   
 function App() {
+  const [show, setShow] = useState(true)
+
   return (
     <div className="App">
-        <NavBar />
-        <CountFunction />
+        <NavBar/>
         <ItemListContainer greeting = "Hello U"/>
+        <ItemCount/>
     </div>
   );
 }
 
 export default App;
+ 
